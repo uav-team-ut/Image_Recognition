807d7f159a9878ee598380b9a8e3dde50cae2e81
Siddarth Kaki <siddarthkaki@utexas.edu>
2014-10-06 21:38:21 -0500

    Renaming files for clarity

---
+++ OpenCV-Test/SimpleCannyEdgeDetection.cpp
@@ -0,0 +1,26 @@
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+//ghjhgkjh
+
+int main()
+{
+	Mat img;
+	VideoCapture cap(0);
+
+	while (true)
+	{
+		cap >> img;
+
+		Mat edges;
+		cvtColor(img, edges, CV_BGR2GRAY);
+
+		Canny(edges, edges, 100, 100);
+
+		imshow("window label", edges);
+		waitKey(1);
+	}
+
+	return 0;
+}
