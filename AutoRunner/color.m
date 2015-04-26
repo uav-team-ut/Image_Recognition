897c0a2ba3d3d9a760c447b3dc06a327cf20f01b
Siddarth Kaki <siddarthkaki@utexas.edu>
2015-04-26 15:36:54 -0500

    Color.m range fix

--- AutoRunner/color.m
+++ AutoRunner/color.m
@@ -1,7 +1,7 @@
 function [color] = color(h,s,v)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-if (h >= 0 && h <= 19) || (h >= 330 && h <= 360)
+if (h >= 0 && h < 20) || (h >= 330 && h <= 360)
     if s >= 0 && s <= 30
         color = 'white';
     elseif v >= 0 && v <= 32
@@ -9,15 +9,15 @@ if (h >= 0 && h <= 19) || (h >= 330 && h <= 360)
     else
         color = 'red';
     end
-elseif h >= 20 && h <= 49
+elseif h >= 20 && h < 50
     color = 'orange';
-elseif h >= 50 && h <= 69
+elseif h >= 50 && h < 70
     color = 'yellow';
-elseif h >= 70 && h <= 170
+elseif h >= 70 && h < 170
     color = 'green';
-elseif h >= 171 && h <= 264
+elseif h >= 170 && h < 265
     color = 'blue';
-elseif h >= 265 && h <= 329
+elseif h >= 265 && h < 330
     color = 'purple';
 else 
     color = 'cannot be determined'
