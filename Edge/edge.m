33537c494365b140688f8afc9782beeae2e72c7e
Siddarth Kaki <siddarthkaki@siddarthkaki-envy-ubuntu>
2015-10-14 18:07:22 -0500

    something 2...

--- Edge/edge.m
+++ Edge/edge.m
@@ -1,12 +1,12 @@
 I = imread('images/crop3.jpg');
 
 
-thresh = multithresh(I,2);
-seg_I = imquantize(I,thresh);
-RGB = label2rgb(seg_I);
-figure;
-imshow(seg_I)
+% thresh = multithresh(I,2);
+% seg_I = imquantize(I,thresh);
+% RGB = label2rgb(seg_I);
+% figure;
+% imshow(seg_I)
 
-% [level EM] = graythresh(I)
-% BW = im2bw(I,level);
-% imshow(BW)
+[level EM] = graythresh(I)
+BW = im2bw(I,level);
+imshow(BW)
