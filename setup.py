4ce466be80fd4d972a5f27682ab473b9fb5f1f53
Rahul Butani <rr.butani@gmail.com>
2017-01-29 15:47:13 -0600

    Added setup.py (for testing purposes)

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+    
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='image-recognition',
+    version='0.1-beta.2',
+    author='Unmaned Aerial Vehicle Team | The University of Texas at Austin',
+    url='https://github.com/uav-team-ut/Image_Recognition',
+    #install_requires=[''],
+    #packages=[''],
+)
